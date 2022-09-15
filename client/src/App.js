d5d322f04802e0a4ed43fb358dd21a7cd8769b8e
kcallaghan1 <kcallaghan@ithaca.edu>
2022-09-15 17:15:05 -0400

    1

---
+++ client/src/App.js
@@ -0,0 +1,95 @@
+import { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation
+} from "react-router-dom";
+import { accessToken, logout, getCurrentUserProfile } from './spotify';
+import { catchErrors } from './utils';
+import { GlobalStyle } from './styles';
+import { Login, Profile, TopArtists, TopTracks, Playlists, Playlist } from './pages';
+import styled from 'styled-components/macro';
+
+
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0,0,0,.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  } 
+`;
+
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0,0);
+  }, [pathname]);
+
+  return null;
+}
+
+function App() {
+  const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    setToken(accessToken);
+
+    const fetchData = async () => {
+      const { data } = await getCurrentUserProfile();
+      setProfile(data);
+    };
+
+    catchErrors(fetchData());
+
+  }, []);
+
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <header className="App-header">
+        {!token ? (
+          <Login />
+        ) : ( 
+          <>
+          <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>
+
+          <Router>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/top-artists" element={
+                <TopArtists /> 
+              } />
+              <Route path="/top-tracks" element={
+                <TopTracks /> 
+              } />
+              <Route path="/playlists/:id" element={
+                <Playlist />
+              } />
+              <Route path="/playlists" element={
+                <Playlists />
+              } />
+              <Route path="/" element={
+                <Profile /> 
+              } />
+            </Routes>
+          </Router>
+          </>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/TopArtists.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import { catchErrors } from '../utils';
+import { SectionWrapper, ArtistsGrid, TimeRangeButtons, Loader } from '../components';
+import { getTopArtists } from '../spotify';
+
+const TopArtists = () => {
+    const [topArtists, setTopArtists] = useState(null);
+    const [activeRange, setActiveRange] = useState('short');
+
+    useEffect(() => {
+        const fetchData = async () => {
+          const userTopArtists = await getTopArtists(`${activeRange}_term`);
+          setTopArtists(userTopArtists.data);
+        };
+
+        catchErrors(fetchData());
+    }, [activeRange]);
+    
+    return(
+      <main>
+        {topArtists ? (
+          <SectionWrapper title="Top artists" breadcrumb="true">
+            <TimeRangeButtons activeRange={activeRange} setActiveRange={setActiveRange} />
+            <ArtistsGrid artists={topArtists.items} />
+          </SectionWrapper>
+        ) : (
+          <Loader />
+        )}
+      </main>
+    );
+}
+
+export default TopArtists;
